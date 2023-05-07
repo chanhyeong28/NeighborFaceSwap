aa17f095ad2e2ad3e9a3982987ce3ce51371de1a
Admin <>
2023-05-07 20:52:26 +0900

    [check]

--- DLpj_models.py
+++ DLpj_models.py
@@ -268,6 +268,7 @@ def process_image_dl(img):
   crop_img_tensor = crop_img_tensor.to(device)
 
   output = model_dl(crop_img_tensor)
+  print(output)
 
   # max_row_index = torch.argmax(output[:, 1])
   # max_row_value = output[max_row_index, 1]
@@ -281,6 +282,7 @@ def process_image_dl(img):
   #     face_ids.append("unknown")
 
   _, preds = torch.max(output, 1)
+  print(preds)
 
   face_ids = []
 
